1bdc55b1d16fa48a5bdef4c5d0c9907b36544666
James Collins <jbccollins@gmail.com>
2019-05-02 01:20:41 -0400

    More minor css fixes

--- client/src/components/SidebarItem/SidebarItem.js
+++ client/src/components/SidebarItem/SidebarItem.js
@@ -1,13 +1,16 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Link } from 'react-scroll'
+import {
+  isMobile,
+} from "react-device-detect";
 import "./SidebarItem.scss";
 
 class SidebarItem extends React.Component {
   render() {
     const { name, link } = this.props;
     return (
-      <Link className="SidebarItem" href={link} to={link} containerId="main" offset={-40} activeClass="selected" spy>       
+      <Link className="SidebarItem" href={link} to={link} containerId="main" offset={isMobile ? -60 : -40} activeClass="selected" spy>       
         <div className="item-content">
           <span>{name}</span>
         </div>
