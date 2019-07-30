a91bf475f9db626d0d913acafcf524784de6e24e
James Collins <jbccollins@gmail.com>
2019-07-30 14:53:06 -0400

    Add real resume

--- bindEndpoints.js
+++ bindEndpoints.js
@@ -67,7 +67,7 @@ const bindEndpoints = app => {
   });
 
   // This does not work locally due to the port redirect. You gotta hit localhost:5001/resume.
-  app.get(RESUME_DOWNLOAD, (req, res) => res.download('./resume.pdf', "James-Collins-Resume.pdf"))
+  app.get(RESUME_DOWNLOAD, (req, res) => res.download('./james_collins_resume_2019_v1.pdf', "James-Collins-Resume.pdf"))
 };
 
 export default bindEndpoints;
